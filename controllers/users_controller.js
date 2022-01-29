8cdb47dce104f0755a40b73d80d0bd508200f4fb
SandeepKumar <sandeep.kharinta200@gmail.com>
2022-01-30 02:39:44 +0530

    create home.ejs file

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,14 @@
 module.exports.profile=function(req,res)
 {
-    return res.end('<h1>User Profile </h1>');
+    return res.render('home',{
+        title:"profile"
+    });
 }
 
 
 module.exports.contact=function(req,res)
 {
-    return res.end('<h1>Users Contact </h1>');
+    return res.render('home',{
+        title:"contact"
+    });
 }
