5c2a1676faa052e24ecdd987d5578696f08e4bc8
SandeepKumar <sandeep.kharinta200@gmail.com>
2022-02-01 03:07:20 +0530

    install layout and set layout and partials

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();//running the express function
 const port=8000;  //when we deploy our product then we change our port to 80
+//require the layouts
+const expressLayout=require('express-ejs-layouts');
+//now saying to express to use this layout before the router
+app.use(expressLayout);
 
 //use express router
 app.use('/',require('./routes/index'));
