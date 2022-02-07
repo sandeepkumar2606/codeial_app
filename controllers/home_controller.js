f3e8fa45fd9dd25a471dd6e365ea2dd501484d0d
SandeepKumar <sandeep.kharinta200@gmail.com>
2022-02-08 00:49:34 +0530

    add user signup

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,8 @@
 
 module.exports.home=function(req,res)
 {
+    console.log(req.cookies);
+    res.cookie('user_id',24);
     return res.render('home',{
         title:"Home"
     });
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
+
 const app=express();//running the express function
 const port=8000;  //when we deploy our product then we change our port to 80
 //require the layouts
 const expressLayout=require('express-ejs-layouts');
+
+//importing mongoose file for database
+const db=require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 //now saying to express to use this layout before the router
 app.use(expressLayout);
 
